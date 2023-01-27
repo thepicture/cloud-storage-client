648938953ebb04c8399061b24ff85f030dfd772c
thepicture <85333076+thepicture@users.noreply.github.com>
2023-01-26 20:18:14 -0500

    feat(folders): implement folder create

--- pages/folders.vue
+++ pages/folders.vue
@@ -11,6 +11,50 @@
         hide-details
       ></v-text-field>
     </v-card-title>
+    <v-dialog v-model="dialog" max-width="500px">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
+          New Folder
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">Add a New Folder</span>
+        </v-card-title>
+
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  v-model="editedFolder.name"
+                  label="Folder Name"
+                  :rules="[
+                    (v) => !!v || 'Folder Name is Required',
+                    (v) =>
+                      folders.every((folder) => folder.name !== v) ||
+                      'This Folder Already Exists',
+                  ]"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="close"> Cancel </v-btn>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="save"
+            :disabled="!canSaveFolder"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <v-select
       :items="viewTypes"
       label="View Type"
@@ -24,9 +68,13 @@
       :items="humanReadableFolders"
       :items-per-page="15"
       class="elevation-1"
+      actions=""
       :search="search"
       v-if="showAs === 'table'"
-    ></v-data-table>
+      ><template v-slot:item.createdAt="{ item }">
+        {{ new Date(item.createdAt).toLocaleString() }}
+      </template></v-data-table
+    >
     <section class="section" v-else>
       <v-card
         class="card mb-4 mt-4"
@@ -39,7 +87,7 @@
         </v-card-title>
         <v-card-subtitle>
           Created by {{ folder.owner }} at
-          {{ folder.createdAt }}
+          {{ new Date(folder.createdAt).toLocaleDateString() }}
         </v-card-subtitle>
         <v-card-text>
           {{ folder.filesCount }}
@@ -105,12 +153,23 @@ export default {
     showAs: 'table',
     viewTypes: ['table', 'grid'],
     search: '',
+    defaultFolder: {
+      name: '',
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0,
+    },
+    editedFolder: {
+      name: '',
+    },
+    dialog: false,
   }),
   computed: {
     humanReadableFolders() {
       return this.folders.map((folder) => ({
         ...folder,
-        createdAt: new Date(folder.createdAt).toLocaleDateString(),
+        createdAt: folder.createdAt,
         totalSizeInBytes: filesize(folder.totalSizeInBytes, {
           base: 2,
           standard: 'jedec',
@@ -126,10 +185,35 @@ export default {
         JSON.stringify(folder).toLowerCase().includes(this.search.toLowerCase())
       )
     },
+    canSaveFolder() {
+      return (
+        !!this.editedFolder.name &&
+        this.folders.every((folder) => folder.name !== this.editedFolder.name)
+      )
+    },
   },
   methods: {
     handleViewTypeSelect() {
       this.$cookies.set(CONSTANTS.FOLDERS_VIEW_TYPE, this.showAs)
+    },
+    save() {
+      this.folders = [
+        ...this.folders,
+        {
+          ...this.editedFolder,
+          createdAt: Number(new Date()),
+          owner: 'You',
+          filesCount: 0,
+          totalSizeInBytes: 0,
+        },
+      ]
+
+      this.close()
+    },
+    close() {
+      this.editedFolder = Object.assign({}, this.defaultFolder)
+
+      this.dialog = false
     },
   },
   created() {
