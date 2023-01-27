ddd44f146a6e2a3f6d30d655eebf37cbfcf7747d
thepicture <85333076+thepicture@users.noreply.github.com>
2023-01-26 20:54:48 -0500

    feat(folders): implement edit/delete

--- pages/folders.vue
+++ pages/folders.vue
@@ -2,6 +2,7 @@
   <v-card class="pl-4 pr-4">
     <v-card-title>
       Folders
+      <v-card-subtitle>{{ totalSizeOfFolders }} Total</v-card-subtitle>
       <v-spacer></v-spacer>
       <v-text-field
         v-model="search"
@@ -19,7 +20,9 @@
       </template>
       <v-card>
         <v-card-title>
-          <span class="text-h5">Add a New Folder</span>
+          <span class="text-h5"
+            >{{ isEditMode ? 'Edit Existing' : 'Add a New' }} Folder</span
+          >
         </v-card-title>
 
         <v-card-text>
@@ -65,20 +68,30 @@
     ></v-select>
     <v-data-table
       :headers="headers"
-      :items="humanReadableFolders"
+      :items="folders"
       :items-per-page="15"
       class="elevation-1"
       actions=""
       :search="search"
       v-if="showAs === 'table'"
-      ><template v-slot:item.createdAt="{ item }">
-        {{ new Date(item.createdAt).toLocaleString() }}
-      </template></v-data-table
     >
+      <template v-slot:item.createdAt="{ item }">
+        {{ new Date(item.createdAt).toLocaleString() }}
+      </template>
+      <template v-slot:item.totalSizeInBytes="{ item }">
+        {{ item.totalSizeInBytes | prettifyBytes }}
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <v-icon small class="mr-2" @click="editFolder(item)">
+          mdi-pencil
+        </v-icon>
+        <v-icon small @click="deleteFolder(item)"> mdi-delete </v-icon>
+      </template>
+    </v-data-table>
     <section class="section" v-else>
       <v-card
         class="card mb-4 mt-4"
-        :key="folder.createdAt + folder.owner"
+        :key="folder.name"
         v-for="folder of filteredGridFolders"
       >
         <v-icon size="300">mdi-folder</v-icon>
@@ -94,8 +107,17 @@
           {{ `file${folder.filesCount === 1 ? '' : 's'}` }}
         </v-card-text>
         <v-card-text>
-          {{ folder.totalSizeInBytes }}
+          {{ folder.totalSizeInBytes | prettifyBytes }}
         </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn outlined color="primary" @click="editFolder(folder)"
+            >Edit</v-btn
+          >
+          <v-btn outlined color="red" @click="deleteFolder(folder)"
+            >Delete</v-btn
+          >
+        </v-card-actions>
       </v-card>
     </section>
   </v-card>
@@ -138,6 +160,7 @@ const MOCK_FOLDERS = new Array(16)
     },
   ])
   .reduce((a1, a2) => [...a1, ...a2], [])
+  .map((folder, index) => ({ ...folder, name: `${folder.name}_${index}` }))
 
 export default {
   name: 'FoldersPage',
@@ -148,6 +171,7 @@ export default {
       { text: 'Owner', value: 'owner' },
       { text: 'Files Count', value: 'filesCount' },
       { text: 'Total Size', value: 'totalSizeInBytes' },
+      { text: 'Actions', value: 'actions', sortable: false },
     ],
     folders: MOCK_FOLDERS,
     showAs: 'table',
@@ -155,33 +179,36 @@ export default {
     search: '',
     defaultFolder: {
       name: '',
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
+      createdAt: 0,
+      owner: 'You',
+      filesCount: 0,
+      totalSizeInBytes: 0,
     },
     editedFolder: {
       name: '',
+      createdAt: 0,
+      owner: 'You',
+      filesCount: 0,
+      totalSizeInBytes: 0,
     },
     dialog: false,
+    isEditMode: false,
   }),
-  computed: {
-    humanReadableFolders() {
-      return this.folders.map((folder) => ({
-        ...folder,
-        createdAt: folder.createdAt,
-        totalSizeInBytes: filesize(folder.totalSizeInBytes, {
-          base: 2,
-          standard: 'jedec',
-        }),
-      }))
+  filters: {
+    prettifyBytes(value) {
+      return filesize(value, {
+        base: 2,
+        standard: 'jedec',
+      })
     },
+  },
+  computed: {
     filteredGridFolders() {
       if (!this.search) {
-        return this.humanReadableFolders
+        return this.folders
       }
 
-      return this.humanReadableFolders.filter((folder) =>
+      return this.folders.filter((folder) =>
         JSON.stringify(folder).toLowerCase().includes(this.search.toLowerCase())
       )
     },
@@ -191,28 +218,46 @@ export default {
         this.folders.every((folder) => folder.name !== this.editedFolder.name)
       )
     },
+    totalSizeOfFolders() {
+      return filesize(
+        this.folders
+          .map((folder) => folder.totalSizeInBytes)
+          .reduce((f1, f2) => f1 + f2, 0),
+        {
+          base: 2,
+          standard: 'jedec',
+        }
+      )
+    },
   },
   methods: {
     handleViewTypeSelect() {
       this.$cookies.set(CONSTANTS.FOLDERS_VIEW_TYPE, this.showAs)
     },
     save() {
-      this.folders = [
-        ...this.folders,
-        {
-          ...this.editedFolder,
-          createdAt: Number(new Date()),
-          owner: 'You',
-          filesCount: 0,
-          totalSizeInBytes: 0,
-        },
-      ]
+      this.folders = this.folders.map((folder) => {
+        if (folder.createdAt === this.editedFolder.createdAt) {
+          folder.name = this.editedFolder.name
+        }
+
+        return folder
+      })
 
       this.close()
+    },
+    editFolder(folder) {
+      this.editedFolder = Object.assign({}, folder)
+
+      this.isEditMode = true
+      this.dialog = true
+    },
+    deleteFolder(folder) {
+      this.folders = this.folders.filter((f) => f.name !== folder.name)
     },
     close() {
       this.editedFolder = Object.assign({}, this.defaultFolder)
 
+      this.isEditMode = false
       this.dialog = false
     },
   },
