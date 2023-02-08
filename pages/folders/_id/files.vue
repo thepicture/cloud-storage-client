aaca18fecfce176c99a812aaedec025ee4743deb
thepicture <85333076+thepicture@users.noreply.github.com>
2023-02-07 20:09:08 -0500

    refactor(files): break dialogs into components

--- pages/folders/_id/files.vue
+++ pages/folders/_id/files.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card class="pl-4 pr-4">
+  <v-card class="pl-4 pr-4 pb-4">
     <v-card-title>
       Files
       <v-btn outlined color="primary" dark class="ml-4" @click="$router.go(-1)"
@@ -16,93 +16,32 @@
         class="mb-4"
       ></v-text-field>
     </v-card-title>
-    <v-dialog v-model="editDialog" max-width="500px">
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn color="primary" dark class="mb-2 ml-4" v-bind="attrs" v-on="on">
-          Upload File
-        </v-btn>
-      </template>
-      <v-card>
-        <v-card-title>
-          <span class="text-h5"
-            >{{ isEditMode ? 'Rename' : 'Upload' }} File</span
-          >
-        </v-card-title>
-        <v-form v-model="valid" ref="form" class="mr-4">
-          <v-card-text v-if="isEditMode">
-            <v-container>
-              <v-row>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field
-                    label="File Name"
-                    v-model="newName"
-                    :rules="fileRules"
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-card-text>
-          <v-file-input
-            :rules="fileRules"
-            label="Select File"
-            show-size
-            class="ml-5"
-            v-model="rawFile"
-            @change="handleFileChange"
-            v-else
-          ></v-file-input>
-
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="close"> Cancel </v-btn>
-            <v-btn
-              color="blue darken-1"
-              text
-              @click="save"
-              :disabled="!canSaveFile"
-            >
-              {{ isEditMode ? 'Save' : 'Upload' }}
-            </v-btn>
-          </v-card-actions>
-        </v-form>
-      </v-card>
-    </v-dialog>
-
-    <v-dialog v-model="directLinkDialog" max-width="500px">
-      <v-card>
-        <v-card-title>
-          <span class="text-h5">File Share</span>
-        </v-card-title>
-        <v-container>
-          <v-row>
-            <v-col cols="12" sm="6" md="4">
-              <v-text-field
-                label="Direct Link"
-                v-model="directLink"
-                readonly
-              ></v-text-field>
-            </v-col>
-          </v-row>
-        </v-container>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="copyDirectLinkToClipboard"
-            >Copy To Clipboard</v-btn
-          >
-          <v-btn color="blue darken-1" text @click="closeDirectLinkDialog"
-            >OK</v-btn
-          >
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-
+    <v-btn color="primary" dark class="mb-2 ml-4" @click="uploadFile">
+      Upload File
+    </v-btn>
+    <FileEditDialog
+      :newName="newName"
+      :file="rawFile"
+      :opened="isFileEditDialogOpened"
+      :is-edit-mode="isEditMode"
+      :rules="fileRules"
+      @change="handleFileChange"
+      @close="closeFileEditDialog"
+      @save="saveFile"
+    />
+    <FileShareDialog
+      :opened="isFileShareDialogOpened"
+      :link="directLink"
+      @linkcopy="copyDirectLinkToClipboard"
+      @close="closeFileShareDialog"
+    />
     <v-select
-      :items="viewTypes"
       label="View Type"
       prepend-icon="mdi-table"
-      @input="handleViewTypeSelect"
-      v-model="showAs"
       class="ml-4 mr-4"
+      v-model="showAs"
+      :items="viewTypes"
+      @input="handleViewTypeSelect"
     ></v-select>
     <v-select
       :items="fileTypes"
@@ -220,6 +159,8 @@ import { FileURI } from '@/utils/FileURI'
 
 import { Timestamp } from '@/utils/Timestamp'
 import TooltipButton from '~/components/TooltipButton.vue'
+import FileShareDialog from '~/components/FileShareDialog.vue'
+import FileEditDialog from '~/components/FileEditDialog.vue'
 
 export default {
   name: 'FilesPage',
@@ -237,10 +178,9 @@ export default {
     fileTypes: Object.values(FILE_TYPE),
     fileType: FILE_TYPE.ALL,
     search: '',
-    editDialog: false,
-    directLinkDialog: false,
+    isFileEditDialogOpened: false,
+    isFileShareDialogOpened: false,
     isEditMode: false,
-    valid: false,
     defaultFile: {
       id: 0,
       name: '',
@@ -323,31 +263,17 @@ export default {
       }
       return files
     },
-    canSaveFile() {
-      return this.valid
-    },
+
     fileRules() {
       if (this.isEditMode) {
         return [
           (v) => !!v || 'New Name is Required',
           (v) => (!!v && !v.endsWith('.php')) || '.php cannot be uploaded',
-          (v) =>
-            (!!v &&
-              this.files.every(
-                (file) => FilenameGetter.getFileName(file) !== v
-              )) ||
-            'File with this name already exists',
         ]
       } else {
         return [
           (v) => !!v || 'File Should Be Presented',
           (v) => (!!v && !v.name.endsWith('.php')) || '.php cannot be uploaded',
-          (v) =>
-            (!!v &&
-              this.files.every(
-                (file) => FilenameGetter.getFileName(file) !== this.rawFile.name
-              )) ||
-            'File with this name already exists',
           () =>
             this.editedFile.bytes.length < TWENTY_MEGABYTES ||
             'File Size should be less than 20mb',
@@ -370,9 +296,9 @@ export default {
     handleViewTypeSelect() {
       this.$cookies.set(CONSTANTS.FOLDERS_VIEW_TYPE, this.showAs)
     },
-    async save() {
+    async saveFile(name) {
       if (this.isEditMode) {
-        const [title, extension] = new FileURI(this.newName).getValues()
+        const [title, extension] = new FileURI(name).getValues()
         await this.$axios.patch(`/files/${this.editedFile.id}`, {
           newTitle: title,
           newExtension: extension,
@@ -416,14 +342,14 @@ export default {
           message: `File uploaded!`,
         })
       }
-      this.close()
+      this.closeFileEditDialog()
     },
     editFile(file) {
       this.rawFile = file
       this.editedFile = file
       this.newName = FilenameGetter.getFileName(file)
       this.isEditMode = true
-      this.editDialog = true
+      this.isFileEditDialogOpened = true
     },
     async deleteFile(fileId) {
       await this.$axios.delete(`/files/${fileId}`)
@@ -432,11 +358,11 @@ export default {
         message: 'Deletion successful!',
       })
     },
-    close() {
+    closeFileEditDialog() {
       this.editedFile = Object.assign({}, this.defaultFile)
-      this.valid = false
+      this.isFileValid = false
       this.isEditMode = false
-      this.editDialog = false
+      this.isFileEditDialogOpened = false
       this.rawFile = null
       try {
         this.$refs.form.reset()
@@ -482,10 +408,10 @@ export default {
 
       this.directLink = directLink
 
-      this.directLinkDialog = true
+      this.isFileShareDialogOpened = true
     },
-    closeDirectLinkDialog() {
-      this.directLinkDialog = false
+    closeFileShareDialog() {
+      this.isFileShareDialogOpened = false
       this.directLink = ''
     },
     copyDirectLinkToClipboard() {
@@ -503,9 +429,9 @@ export default {
         })
       }
     },
-    /**
-     * @param {File} file
-     */
+    uploadFile() {
+      this.isFileEditDialogOpened = true
+    },
     async _getBytes(file) {
       const buffer = await file.arrayBuffer()
       const array = new Int8Array(buffer)
@@ -521,7 +447,7 @@ export default {
     )
     this.files = response.data.files
   },
-  components: { TooltipButton },
+  components: { TooltipButton, FileShareDialog, FileEditDialog },
 }
 </script>
 
